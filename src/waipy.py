24c7b26902f223210045b994edc62e1cc0d7f157
Stefan Greve <greve.stefan@outlook.jp>
2020-05-02 22:29:13 +0200

    Completed implementation of the most important main features.
    - Created Bot class to improve ease-of-use
    - Moved multiple bot creation from browse method to init
    - Added progress bar with tqdm

--- src/waipy.py
+++ src/waipy.py
@@ -1,50 +1,53 @@
 #!/usr/bin/env python3
 
-import json
 import functools
 from pathlib import Path
 from urllib.parse import urlparse
 
+import gentools as gen
+
 import praw
 from faker import Faker
+from tqdm import tqdm
+from colorama import Fore
+
+class Bot(object):
+    def __init__(self, filename = "credentials.json", count = 1):
+        self.configuration = filename
+        self.bot = next(self.new_bot(count))
+
+    def new_bot(self, count):
+        config = gen.read_json(self.configuration)
+        
+        if int(config['count']) < count:
+            raise ValueError(f"{self.configuration}でご提示いただいた認証情報は物足りなくありません.")
+        
+        for account in range(count):
+            yield praw.Reddit(
+                client_id = config['bots'][account]['client_id'],
+                client_secret = config['bots'][account]['client_secret'],
+                user_agent = Faker().user_agent()
+            )
+
+    def browse(self, subreddit, target_directory, limit = 10, min_score = 10):
+        subreddit = self.bot.subreddit(subreddit)
+
+        for submission in tqdm(subreddit.top(limit = limit), unit = '画像', desc = 'ダウンロード中', bar_format = "{l_bar}%s{bar}%s{r_bar}" % (Fore.GREEN, Fore.RESET), ascii = True, total = limit):
+            url = urlparse(submission.url)
+            image = target_directory.joinpath(url.path[1:])
+
+            if url.netloc == "i.redd.it" and submission.score >= min_score and not image.exists():              
+                gen.download(submission.url, image)
 
 
-def read_file(file):
-    with open(file, encoding = 'utf-8', mode = 'r') as file:
-        return file.read()
+if __name__ == '__main__': 
+    bot = Bot()
 
-def read_json(file):
-    return json.loads(read_file(file))
-
-def get_credentials(file):
-    return read_json(file)['bots']
-
-def new_bot(file, count = 1):
-    source = read_json(file)
+    bot.browse(
+        subreddit = 'animewallpaper',
+        target_directory = gen.make_onedrive_directory('画像/アニメ/animewallpaper')
+    )
     
-    if int(source['count']) < count:
-        raise ValueError(f"Insufficient bot credentials defined in '{file}'.")
     
-    for account in range(count):
-        yield praw.Reddit(
-            client_id = source['bots'][account]['client_id'],
-            client_secret = source['bots'][account]['client_secret'],
-            user_agent = Faker().user_agent()
-        )
-
-def browser(file, bot_count, limit):
-    for reddit in new_bot(file, bot_count):
-        subreddit = reddit.subreddit('animewallpaper')
-
-        for submission in subreddit.top(limit):
-            if not submission.stickied:
-                url = urlparse(submission.url)
-                if url.netloc == "i.redd.it":
-                    print(submission.url)
-
-def main(file, bot_count, limit):
-    browser(file, bot_count, limit)
-
-if __name__ == '__main__':
-    main('credentials.json', bot_count = 1, limit = 10)
-
+    
+        
